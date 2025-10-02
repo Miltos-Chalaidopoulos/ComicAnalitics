93cedf45a551dc14a0357fbf2f6883cceead12c3
Miltos <147398399+Miltos-Chalaidopoulos@users.noreply.github.com>
2025-10-02 19:47:14 +0300

    Add CSV import/export tests

---
+++ src/services/csv_edit.py
@@ -0,0 +1,35 @@
+import csv
+from database.db_manager import DBManager
+
+
+class CSVService:
+    def __init__(self, db: DBManager):
+        self.db = db
+
+    def import_mickey(self, csv_file):
+        with open(csv_file, newline="", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                self.db.add_mickey(int(row["Issue num"]),int(row["Vol num"]),row["Main Story"],int(row["Year"]))
+
+    def export_mickey(self, csv_file):
+        rows = self.db.search_mickey()
+        with open(csv_file, "w", newline="", encoding="utf-8") as f:
+            writer = csv.DictWriter(f,fieldnames=["Issue num", "Vol num", "Main Story", "Year"])
+            writer.writeheader()
+            for row in rows:
+                writer.writerow({"Issue num": row["issue_num"],"Vol num": row["vol_num"],"Main Story": row["mainstory"],"Year": row["year"]})
+
+    def import_other(self, csv_file):
+        with open(csv_file, newline="", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                self.db.add_other(row["Title"],row["Writer"],row["Artist"],row["Collection"],row["Publisher"],row["Issues"],row["Main Character"],True if row["Event"].lower() == "true" else False,int(row["Story Year"]),row["Category"])
+
+    def export_other(self, csv_file):
+        rows = self.db.search_other()
+        with open(csv_file, "w", newline="", encoding="utf-8") as f:
+            writer = csv.DictWriter(f,fieldnames=["Title", "Writer", "Artist", "Collection","Publisher", "Issues", "Main Character", "Event","Story Year", "Category"])
+            writer.writeheader()
+            for row in rows:
+                writer.writerow({"Title": row["title"],"Writer": row["writer"],"Artist": row["artist"],"Collection": row["collection"],"Publisher": row["publisher"],"Issues": row["issues"],"Main Character": row["main_character"],"Event": "true" if row["event"] else "false","Story Year": row["story_year"],"Category": row["category"]})
